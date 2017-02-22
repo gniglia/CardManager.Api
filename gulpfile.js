701d5af3fc10ba894b40420828eac03f6bf8843a
Gaston Niglia <gniglia@gmail.com>
2017-02-22 22:52:58 +1300

    adding gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+var clean = require('gulp-rimraf');
 
 gulp.task('default', function() {
   console.log('this is the default task');
@@ -12,10 +13,18 @@ gulp.task('dist', function() {
 });
 
 gulp.task('copy-package-file', function() {
+  console.log('Copy package.json file into dist folder');
+
   gulp.src('package.json')
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['dist', 'copy-package-file'], function() {
+gulp.task('clean', function() {
+  console.log('Clean all files in dist folder');
+
+  gulp.src('dist/*', { read: false }).pipe(clean());
+});
+
+gulp.task('build', ['clean', 'dist', 'copy-package-file'], function() {
   console.log('The API has been built...');
 });
